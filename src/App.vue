8c5703d7841a18f6708a414fa69b76e97bceb232
dozyhaha <146948662+dozyhaha@users.noreply.github.com>
2025-10-16 15:57:19 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import MyHeader from '$src/components/MyHeader.vue'
+</script>
+
+<template>
+  <MyHeader :example="{ value: 'Hello Vite + Vue.js + tailwindUI static SPA' }" />
+
+  <nav class="p-4">
+    <a
+      class="mr-4"
+      href="/"
+    >
+      Home
+    </a>
+
+    <a href="/about">
+      About
+    </a>
+  </nav>
+
+  <router-view />
+</template>
